dd97726f44250839238981436533456cb5296aa1
WagnerChicarelli <chicarelli.wagner@gmail.com>
2026-08-18 09:55:05 -0300

    Adicionando atualizacaoes do ultimo commit

--- src/server.js
+++ src/server.js
@@ -1,13 +1,13 @@
 import express from 'express';
 import path from 'node:path';
-import {fileURLtoPath} from 'node:url';
+import {fileURLToPath} from 'node:url';
 import {createPlant, isDueForWatering, waterPlant} from './plant.js';
 import {loadPlants, savePlants} from './storage.js';
 
 const app = express();
 const port = 3000;
 
-const currentDirectory = path.dirname(fileURLtoPath(import.meta.url));
+const currentDirectory = path.dirname(fileURLToPath(import.meta.url));
 const dataFile = path.join(currentDirectory, '..','data','plants.json');
 
 app.use(express.json());
@@ -17,12 +17,12 @@ app.get('/plants', async (req,res) => {
     res.json(plants);
 });
 
-app.post('/plants/due-for-watering', async (req,res) => {
+app.get('/plants/due-for-watering', async (req,res) => {
     const plants = await loadPlants(dataFile);
     res.json(plants.filter(isDueForWatering));
 });
 
-app.post('/plants/:id', async (req,res) => {
+app.get('/plants/:id', async (req,res) => {
     const plants = await loadPlants(dataFile);
     const plant = plants.find(p => p.id === req.params.id);
 
@@ -36,14 +36,14 @@ app.post('/plants/:id', async (req,res) => {
 
 app.post('/plants/:id/water', async (req,res) => {
     const plants = await loadPlants(dataFile);
-    const plant= plants.find(p => p.id === req.params.id);
+    const index = plants.findIndex(p => p.id === req.params.id);
 
-    if (!plant) {
+    if (index === -1) {
         res.status(404).json({ error: 'Planta não encontrada.' });
         return;
     }
 
-    plant[index] = waterPlant(plant[index], req.body);
+    plants[index] = waterPlant(plants[index], req.body);
     await savePlants(dataFile, plants);
 
     res.json(plant[index]);
